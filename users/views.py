1bd0d2d409cab2727b7cbab69eae9f5ceee867b3
JJooo <bough157@gmail.com>
2025-02-16 20:40:16 +0900

    Revert "Revert "Revert "#4 [feat] django 초기 셋팅 완료"""
    This reverts commit 3aa7f579a95543d7a5fde0de5796d642a0b5080d.

--- users/views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import generics
-
-from .serializers import RegisterSerializer
-
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
